22f0d4b3b4d3fbe258164d4f08c6e5b7f0947834
Alec White <alexander.k.white@gmail.com>
2023-11-25 14:51:43 -0800

    Initial Commit

---
+++ sous/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sous
+//
+//  Created by Alexander K White on 11/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sous/sousApp.swift
@@ -0,0 +1,17 @@
+//
+//  sousApp.swift
+//  sous
+//
+//  Created by Alexander K White on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct sousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
